b6413bd1c27d5cc96e83fcb925caa95f072970fd
tasoskoutlis <tasos.koutlis@gmail.com>
2016-07-31 15:36:08 +0100

    update - recycle bin - minor changes/fixes in rule1

--- rules.py
+++ rules.py
@@ -52,24 +52,36 @@ def timeCreated(timestamp1, timestamp2):
     
 
 def event(results):
-   
+    
+    eventArray = []
     minV = [] 
     while(results):
+        t1 = datetime.strptime(results[0][1], "%Y-%m-%d %H:%M:%S.%f")
+        minV = [t1, 0, 1]
         for i in xrange(len(results)):
             if len(results[i]) > 2:
-                t1 = datetime.strptime(results[0][3], "%Y-%m-%d %H:%M:%S.%f")
-                minV = [t1, 0, 3]
-                for j in range(3, 10, 2):
+                for j in range(1, len(results[i]), 2):
                     t2 = datetime.strptime(results[i][j], "%Y-%m-%d %H:%M:%S.%f")
                     if t2 == min(t1,t2):
+                        t1 = t2
                         minV = [t2, i, j]
             else:
-                print i
                 t2 = datetime.strptime(results[i][1], "%Y-%m-%d %H:%M:%S.%f")
                 if t2 == min(t1,t2):
+                    t1 = t2
                     minV = [t2, i, 1]
-                       
-    print 'Print the min ', results[minV[1]][minV[2]]
+
+        #A few of the entries (Registry) store a newline at the end so I remove it
+        eventArray.append(results[minV[1]][minV[2]-1].rstrip('\n'))
+        eventArray.append(results[minV[1]][minV[2]])
+        
+        if len(results[minV[1]]) > 2:            
+            results[minV[1]].pop(minV[2])
+            results[minV[1]].pop(minV[2]-1)
+        else:
+            results.pop(minV[1])
+            
+    return eventArray
 
 
 def registryInfo(f):
@@ -119,43 +131,47 @@ def mft(f):
                 
     return eventArray
     
+def searchRecycleBin(mftArray):
+    ''' Reads mft.csv file and extracts info to an array each row represents information about a file
+        f               - file
+        @eventArray     - return array with results
+    '''
+    recycleBinArray = []
+    cnt = 0
+    for i in xrange(len(mftArray)):
+        if 'Recycle.Bin' in mftArray[i][7]:
+            recycleBinArray.append([]) 
+            filename = mftArray[i][7]          
+            for j in range(8,12):
+                #format is ['path str_time, timestamp,...] - [text.txt Std Info Access Date, 2015-01-02 22:49:35.829651]
+                recycleBinArray[cnt].append(filename + ' ' + mftArray[0][j])
+                recycleBinArray[cnt].append(mftArray[i][j])
+            cnt += 1
+                
+    return recycleBinArray
+    
 def searchFile(name, mftArray, userAssist, recents):
     results = []
     cnt = 0
     
     #Search for a specific file name in the MFT table
     for i in xrange(len(mftArray)):
-        if 'RecycleTestDocument' in mftArray[i][7]:
-            results.append([])            
-            for j in range(7,12):
-                #format is ['filename', path, 'str', timestamp,...]
-                results[cnt].append(mftArray[0][j])
+        if name in mftArray[i][7]:
+            results.append([]) 
+            filename = mftArray[i][7]          
+            for j in range(8,12):
+                #format is ['path str_time, timestamp,...] - [text.txt Std Info Access Date, 2015-01-02 22:49:35.829651]
+                results[cnt].append(filename + ' ' + mftArray[0][j])
                 results[cnt].append(mftArray[i][j])
             cnt += 1
-     
-    for i in xrange(len(userAssist)):
-        timestamp = userAssist[i][3]
-        length = len(results)
-        for j in xrange(length):
-            try:
-                datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
-            except:
-                #It will hit the exception when it finds 1601-01-01 00:00:00
-                continue
-            if timeCreated(timestamp, results[j][3]) == 0:
-                results.append([])            
-                results[cnt].append(userAssist[i][4])
-                results[cnt].append(userAssist[i][3])
-                cnt += 1
-                
-                #BREAK MIGHT CAUSE PROBLEMS IN THE FUTURE!!!!!!!!MIGHT LOSE OTHER EVENTS
-                break
-                       
-    print 'Moving to Checking All'
+            
+    #print 'Moving to Checking All'
     
+    size = len(results)
     for i in xrange(len(userAssist)):
         timestamp = userAssist[i][3]
-        for j in range(3, 10, 2):
+        #for k in xrange(size):
+        for j in range(1, 7, 2):
             try:
                 datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
             except:
@@ -167,10 +183,39 @@ def searchFile(name, mftArray, userAssist, recents):
                 results[cnt].append(userAssist[i][3])
                 cnt += 1
                 break
-                 
-    print results
+     
+    #print 'Moving to Created'
+     
+    for i in xrange(len(userAssist)):
+        timestamp = userAssist[i][3]
+        for j in xrange(size):
+            try:
+                datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+            except:
+                #It will hit the exception when it finds 1601-01-01 00:00:00
+                continue
+            if timeCreated(timestamp, results[j][1]) == 0:
+                results.append([])            
+                results[cnt].append(userAssist[i][4])
+                results[cnt].append(userAssist[i][3])
+                cnt += 1
+                #BREAK MIGHT CAUSE PROBLEMS IN THE FUTURE!!!!!!!!MIGHT LOSE OTHER EVENTS
+                break
     
-    event(results)
+    #Run results of 1st Rule
+    eventArray = event(results)
+        
+    
+    for i in range(0, len(eventArray), 2):
+        print '%s  ----  %s ' % (eventArray[i], eventArray[i+1])
+    
+    
+def searchTimeFrame(name, mftArray, userAssist, recents):
+    print
+    
+    
+    
+    
 
 def main():
     
@@ -189,12 +234,19 @@ def main():
     recents = registryInfo(f)
     f.close()
     
+    #Search the Recycle Bin entries for information
+    recycleBin = searchRecycleBin(mftArray)
+    
+    print recycleBin
+    print
+    
     name = 'RecycleTestDocument'
     
     #Rule 1 - Search everything to find information about a specific file
     searchFile(name, mftArray, userAssist, recents)
     
-    
+    #Rule 2 - Everything that occurred in a given time frame
+    searchTimeFrame(name, mftArray, userAssist, recents)
     
 
 if __name__ == "__main__":
